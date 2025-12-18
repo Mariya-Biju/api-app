fab5a7b16cd007f668bdbba763acdb26600d902f
Mariya Biju <bijumariya38@gmail.com>
2025-12-18 17:37:27 +0530

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,37 +11,35 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function facultyLogin(Request $request)
-{
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-    $record = DB::table('users')
-                ->where('email', $request->email)
-                ->first();
+        $record = DB::table('users')
+            ->where('email', $request->email)
+            ->first();
 
-    if (! $record) {
+        if (! $record) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        if (! Hash::check($request->password, $record->password)) {
+            return "incorect password";
+        }
+
+        $user = User::find($record->user_id);
+
+        $token = $user->createToken('API Token')->accessToken;
+
         return response()->json([
-            'success' => false,
-            'message' => 'User not found'
-        ], 404);
+            'success' => true,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 200);
     }
-
-    if (! Hash::check($request->password, $record->password)) {
-        return "incorect password";
-    }
-
-    $user = User::find($record->user_id);
-
-    $token = $user->createToken('API Token')->accessToken;
-
-    return response()->json([
-        'success' => true,
-        'access_token' => $token,
-        'token_type' => 'Bearer',
-    ], 200);
-}
-
-
 }
